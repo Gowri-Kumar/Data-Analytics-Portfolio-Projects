723dd2e217176effcc6df7ddd22928a53a8eb7b9
Gowri-Kumar <gowriskkumar@gmail.com>
2024-10-11 10:46:04 +1300

    Delete modules directory

--- modules/databricks/outputs.tf
+++
@@ -1,2 +0,0 @@
-output "workspace_url" { value = azurerm_databricks_workspace.databricks_ws.workspace_url }
-# output "metastores" { value = data.databricks_metastores.this.id }
--- modules/lakehouse/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "resource_group_name" { type = string }
-variable "az_location" { type = string }
-
